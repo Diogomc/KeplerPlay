7c1761e8364293f87bc2add45591e8230ea84930
Diogo <diogo.marcondes2004@gmail.com>
2025-03-16 22:32:31 -0300

    plans area added, and mobile created

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,13 +3,14 @@
 import { Home } from "@/components/home/Home";
 import { Nav } from "@/components/nav/Nav";
 import { Main } from "@/components/main/Main";
+import { Plans } from "@/components/plans/Plans";
 
 export default function Page() {
   return (
     <div>
       <Nav />
       <Main />
-     
+     <Plans/>
       <Home />
 
     </div>
--- src/components/plans/Plans.tsx
+++ src/components/plans/Plans.tsx
@@ -2,7 +2,58 @@ import '@/components/plans/plans.css';
 export const Plans = () => {
     return (
         <div className="container-plans">
-            <h1 >Conheça nossos planos</h1>
+            <h2 className='text-5xl p-20 text-center'>Conheça nossos planos</h2>
+            <div className="plans-list">
+
+                <div className='plans-cards'>
+                    <h2 className='title-plan-card'>Semanal</h2>
+                    <div className="benefits">
+                        <p className='benefits-text'>2 dispositivos ao mesmo tempo</p>
+                        <p className='benefits-text'>Resolução full hd</p>
+                    </div>
+                    <div className="price">
+                        <p className='price-text'>2x R$15,90</p>
+                        <p className='price-text'>Preço total a vista R$27,90</p>
+                        <div className="flex justify-center">
+                            <button className="btn-plan"><a href="#">Escolher esse plano</a></button>
+                        </div>
+                    </div>
+                </div>
+
+                <div className='plans-cards'>
+                    <h2 className='title-plan-card'>Mensal</h2>
+                    <div className="benefits">
+                        <p className='benefits-text'>2 dispositivos ao mesmo tempo</p>
+                        <p className='benefits-text'>Resolução full hd</p>
+                        <p className='benefits-text'>Acesso ao Brasileirão</p>
+                    </div>
+                    <div className="price">
+                        <p className='price-text'>2x R$25,90</p>
+                        <p className='price-text'>Preço total a vista R$47,90</p>
+                        <div className="flex justify-center">
+                            <button className="btn-plan"><a href="#">Escolher esse plano</a></button>
+                        </div>
+                    </div>
+                </div>
+
+                <div className='plans-cards'>
+
+                    <h2 className='title-plan-card'>Anual</h2>
+                    <div className="benefits">
+                        <p className='benefits-text'>5 dispositivos ao mesmo tempo</p>
+                        <p className='benefits-text'>Resolução 4K</p>
+                        <p className='benefits-text'>Acesso ao Brasileirão</p>
+                        <p className='benefits-text'>Acesso a Champions League</p>
+                    </div>
+                    <div className="price">
+                        <p className='price-text'>12x R$42,50<span className='month'>/mês</span></p>
+                        <p className='price-text'>Preço total a vista R$480,00</p>
+                        <div className="flex justify-center">
+                            <button className="btn-plan"><a href="#">Escolher esse plano</a></button>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
